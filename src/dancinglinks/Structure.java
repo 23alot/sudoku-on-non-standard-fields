2b4d5eee54ba522151e274163a7aab9718aa31e2
Boscatov <ayumamontov@gmail.com>
2017-12-02 12:20:11 +0300

    Add Structure class

---
+++ src/dancinglinks/Structure.java
@@ -0,0 +1,15 @@
+package dancinglinks;
+
+/**
+ * Created by boscatov on 02.12.2017.
+ */
+public class Structure {
+
+    /**
+     *
+     * @param N dimension of Sudoku
+     */
+    Structure(byte N){
+        // TODO: create base structure with height N^3 and width 4*N^2
+    }
+}
