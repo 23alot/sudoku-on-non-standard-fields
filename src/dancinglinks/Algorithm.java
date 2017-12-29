9ace5b6b95b514681cf88de219253188af4e6c6b
Boscatov <ayumamontov@gmail.com>
2017-12-29 19:14:38 +0300

    Add rough implementation of solve function

--- src/dancinglinks/Algorithm.java
+++ src/dancinglinks/Algorithm.java
@@ -4,26 +4,39 @@ package dancinglinks;
  * Created by boscatov on 02.12.2017.
  */
 public class Algorithm {
-    HeadNode root;
-    byte minLength;
-    HeadNode minNode;
+    Structure structure;
 
     /**
      *
      * @param N dimension of Sudoku
      */
     Algorithm(byte N){
-        minLength = N;
-        minNode = (HeadNode)root.right;
+
     }
 
+    /**
+     * Recursive function that finds solution
+     */
+    void solve(){
+        HeadNode deleted = structure.minNode;
+        Node temp = deleted.down;
+        for(byte i = 0; i < deleted.currentNumber; ++i){
+            delete(temp);
+            solve();
+            cover(temp);
+        }
+    }
+    void cover(Node nd){
+
+    }
     /**
      * Deletes from structure each solution
      * @param nd head node of column to delete
      */
-    void delete(HeadNode nd){
+    void delete(Node nd){
         Node temp = nd.down;
-        for(byte i = 0; i < nd.currentNumber; ++i){
+        HeadNode head = nd.upHead;
+        for(byte i = 0; i < head.currentNumber; ++i){
             deleteRow(temp);
             temp = temp.down;
         }
@@ -41,9 +54,9 @@ public class Algorithm {
         Node temp = head.right;
         for(byte i = 0; i < head.currentNumber; ++i){
             temp.up.upHead.currentNumber--;
-            if(temp.up.upHead.currentNumber < minLength){
-                minLength = temp.up.upHead.currentNumber;
-                minNode = temp.up.upHead;
+            if(temp.up.upHead.currentNumber < structure.minLength){
+                structure.minLength = temp.up.upHead.currentNumber;
+                structure.minNode = temp.up.upHead;
             }
             temp.up.down = temp.down.up;
             temp.down.up = temp.up.down;
