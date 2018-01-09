02b69024e6801ad630ce159f5f210a8f027ad195
Boscatov <ayumamontov@gmail.com>
2018-01-09 10:30:39 +0300

    Add from input constructor

--- src/sudoku/Board.java
+++ src/sudoku/Board.java
@@ -7,9 +7,18 @@ public class Board {
     public Cell[][] cells;
     public byte[] areas;
     public byte N;
+    public byte emptyCells;
     public Board(byte N, byte[] areas){
         this.N = N;
         this.areas = areas;
         this.cells = new Cell[N][N];
+        this.emptyCells = (byte)(N*N);
+    }
+    public Board(Byte N, byte[] areas, byte[] input){
+        this(N,areas);
+        for(int i = 0; i < input.length; ++i){
+            cells[i/N][i%N].value = input[i];
+            emptyCells--;
+        }
     }
 }
