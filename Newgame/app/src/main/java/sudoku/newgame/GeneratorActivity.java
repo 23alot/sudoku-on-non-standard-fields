2215d6bab31752f86517b940e3a349e543635d7d
Boscatov <ayumamontov@gmail.com>
2018-01-17 00:18:58 +0300

    Changing activity is too long

--- Newgame/app/src/main/java/sudoku/newgame/GeneratorActivity.java
+++ Newgame/app/src/main/java/sudoku/newgame/GeneratorActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
