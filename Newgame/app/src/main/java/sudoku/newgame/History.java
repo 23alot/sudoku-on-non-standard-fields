bba1638f8114336933e84b29b85f4db8fd6c9aa3
Boscatov <ayumamontov@gmail.com>
2018-02-03 18:26:17 +0300

    Add rough constructor and method add event
    addEvent is called when user change any value on board

---
+++ Newgame/app/src/main/java/sudoku/newgame/History.java
@@ -0,0 +1,14 @@
+package sudoku.newgame;
+
+/**
+ * Created by sanya on 03.02.2018.
+ */
+
+public class History {
+    public History(){
+
+    }
+    public void addEvent(int value, boolean isPen, boolean isEnter){
+
+    }
+}
