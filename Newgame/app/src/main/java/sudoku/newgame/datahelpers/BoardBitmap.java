39b8e74a603899f9808cfd80424c82dde081e8a7
Boscatov <ayumamontov@gmail.com>
2018-03-03 23:18:07 +0300

    Add relationship between canvas and bitmap

--- Newgame/app/src/main/java/sudoku/newgame/datahelpers/BoardBitmap.java
+++ Newgame/app/src/main/java/sudoku/newgame/datahelpers/BoardBitmap.java
@@ -1,5 +1,9 @@
 package sudoku.newgame.datahelpers;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 import sudoku.newgame.draw.DrawBoard;
 
 /**
@@ -8,8 +12,17 @@ import sudoku.newgame.draw.DrawBoard;
 
 public class BoardBitmap {
     DrawBoard db;
+    int len;
     public BoardBitmap(byte[] structure, int n, float length) {
-        float size = (length - n*10)/2/n;
+        len = (int) (length - n * 10) >> 1;
+        float size = len / n;
         db = new DrawBoard(size, structure, n);
+    }
+    public void toBitmap() {
+        Bitmap myBitmap = Bitmap.createBitmap( len, len, Bitmap.Config.RGB_565 );
+        Canvas canvas = new Canvas();
+        canvas.setBitmap(myBitmap);
+        Paint p = new Paint();
+        db.drawBitmap(canvas, p);
     }
 }
