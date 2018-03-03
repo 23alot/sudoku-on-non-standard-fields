747d7f2d7772f3fbbdbb195d31ec3ebf30ff762b
Boscatov <ayumamontov@gmail.com>
2018-03-03 23:56:44 +0300

    Add functions to save files

--- Newgame/app/src/main/java/sudoku/newgame/datahelpers/BoardBitmap.java
+++ Newgame/app/src/main/java/sudoku/newgame/datahelpers/BoardBitmap.java
@@ -1,8 +1,18 @@
 package sudoku.newgame.datahelpers;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 import sudoku.newgame.draw.DrawBoard;
 
@@ -24,5 +34,68 @@ public class BoardBitmap {
         canvas.setBitmap(myBitmap);
         Paint p = new Paint();
         db.drawBitmap(canvas, p);
+    }
+    public void save(Context context, Bitmap bitmap) {
+        File directory = context.getFilesDir();
+        File[] files = directory.listFiles();
+        String[] names = new String[files.length];
+        for(int i = 0; i < files.length; ++i) {
+            try {
+                names[i] = files[i].getCanonicalPath();
+            }
+            catch (IOException ex){
+                Log.d("GetBitmapFiles", ex.getMessage());
+            }
+
+        }
+        Arrays.sort(names);
+        rearrangeFiles(names, context);
+
+    }
+    private void rearrangeFiles(String[] names, Context context) {
+        for(int i = names.length-2; i >= 0; --i) {
+            String path = context.getFilesDir() + "/" + names[i];
+            InputStream is = null;
+            try {
+                is = context.openFileInput(path);
+            }
+            catch (FileNotFoundException ex) {
+                Log.d("RearrangeBitmapFiles", ex.getMessage());
+            }
+            Bitmap b = BitmapFactory.decodeStream(is);
+            path = names[i+1];
+            File mypath=new File(context.getFilesDir(), path);
+
+            FileOutputStream fos = null;
+            try {
+                fos = new FileOutputStream(mypath);
+                b.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    private void saveBitmap(String name,Bitmap b, Context context) {
+        File mypath=new File(context.getFilesDir(), name);
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            b.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
