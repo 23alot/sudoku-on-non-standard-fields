809cc625f9dd430328e1bc9b66f58fe8e2ecbbf1
Boscatov <ayumamontov@gmail.com>
2018-03-04 20:15:04 +0300

    End implementation of last created boards

--- Newgame/app/src/main/java/sudoku/newgame/BoardsActivity.java
+++ Newgame/app/src/main/java/sudoku/newgame/BoardsActivity.java
@@ -1,10 +1,14 @@
 package sudoku.newgame;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.ImageView;
+
+import sudoku.newgame.datahelpers.BoardBitmap;
 
 /**
  * Created by sanya on 04.03.2018.
@@ -15,6 +19,18 @@ public class BoardsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board_layout);
-
+        setupBitmap();
+    }
+    private void setupBitmap() {
+        BoardBitmap board = new BoardBitmap();
+        Bitmap[] boards = board.getBitmap(this);
+        if(boards == null)
+            return;
+        for(int i = 0; i < boards.length; ++i) {
+            String name = "imageView"+(i+1);
+            int resID = this.getResources().getIdentifier(name, "id", this.getPackageName());
+            ImageView img = findViewById(resID);
+            img.setImageBitmap(boards[i]);
+        }
     }
 }
--- Newgame/app/src/main/java/sudoku/newgame/datahelpers/BoardBitmap.java
+++ Newgame/app/src/main/java/sudoku/newgame/datahelpers/BoardBitmap.java
@@ -8,6 +8,7 @@ import android.graphics.Paint;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -15,6 +16,7 @@ import java.io.InputStream;
 import java.util.Arrays;
 
 import sudoku.newgame.draw.DrawBoard;
+import sudoku.newgame.sudoku.Board;
 
 /**
  * Created by sanya on 03.03.2018.
@@ -29,16 +31,15 @@ public class BoardBitmap {
         float size = len / n;
         db = new DrawBoard(size, structure, n);
     }
-    public void toBitmap() {
-        myBitmap = Bitmap.createBitmap( len, len, Bitmap.Config.RGB_565 );
-        Canvas canvas = new Canvas();
-        canvas.setBitmap(myBitmap);
-        Paint p = new Paint();
-        db.drawBitmap(canvas, p);
+    public BoardBitmap() {
+
     }
-    public void save(Context context) {
-        File directory = context.getFilesDir();
+
+    public Bitmap[] getBitmap(Context context) {
+        File directory = context.getDir("boards", Context.MODE_PRIVATE);
         File[] files = directory.listFiles();
+        if(files == null)
+            return null;
         String[] names = new String[files.length];
         for(int i = 0; i < files.length; ++i) {
             try {
@@ -47,56 +48,93 @@ public class BoardBitmap {
             catch (IOException ex){
                 Log.d("GetBitmapFiles", ex.getMessage());
             }
-
         }
         Arrays.sort(names);
-        rearrangeFiles(names, context);
-        saveBitmap(names[0], myBitmap, context);
-    }
-    private void rearrangeFiles(String[] names, Context context) {
-        for(int i = names.length-2; i >= 0; --i) {
-            String path = context.getFilesDir() + "/" + names[i];
-            InputStream is = null;
+        Bitmap[] result = new Bitmap[names.length];
+        for(int i = 0; i < names.length; ++i) {
             try {
-                is = context.openFileInput(path);
+                File f = new File(context.getDir("boards", Context.MODE_PRIVATE), "board"+i+".bmp");
+                result[i] = BitmapFactory.decodeStream(new FileInputStream(f));
             }
             catch (FileNotFoundException ex) {
                 Log.d("RearrangeBitmapFiles", ex.getMessage());
             }
-            Bitmap b = BitmapFactory.decodeStream(is);
-            path = names[i+1];
-            File mypath=new File(context.getFilesDir(), path);
-
+            //result[i] = BitmapFactory.decodeStream(is);
+        }
+        return result;
+    }
+    public void toBitmap() {
+        myBitmap = Bitmap.createBitmap( len, len, Bitmap.Config.RGB_565 );
+        Canvas canvas = new Canvas();
+        canvas.setBitmap(myBitmap);
+        Paint p = new Paint();
+        db.drawBitmap(canvas, p);
+    }
+    public void save(Context context) {
+        File directory = context.getDir("boards", Context.MODE_PRIVATE);
+        File[] files = directory.listFiles();
+        if(files != null) {
+            String[] names = new String[files.length];
+            for (int i = 0; i < files.length; ++i) {
+                try {
+                    names[i] = files[i].getCanonicalPath();
+                } catch (IOException ex) {
+                    Log.d("GetBitmapFiles", ex.getMessage());
+                }
+            }
+            Arrays.sort(names);
+            rearrangeFiles(names, context);
+        }
+        saveBitmap("board0.bmp", myBitmap, context);
+    }
+    private void rearrangeFiles(String[] names, Context context) {
+        int size = names.length == 9?names.length-2:names.length-1;
+        for(int i = size; i >= 0; --i) {
             FileOutputStream fos = null;
             try {
-                fos = new FileOutputStream(mypath);
-                b.compress(Bitmap.CompressFormat.PNG, 100, fos);
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
+                File f = new File(context.getDir("boards", Context.MODE_PRIVATE),"board"+i+".bmp");
+                Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+                File fn = new File(context.getDir("boards", Context.MODE_PRIVATE),"board"+(i+1)+".bmp");
+                fos = new FileOutputStream(fn);
+                b.compress(Bitmap.CompressFormat.JPEG,100, fos);
+            }
+            catch (FileNotFoundException ex) {
+                Log.d("RearrangeBitmapFiles", ex.getMessage());
+            }
+            finally {
                 try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    if(fos != null) {
+                        fos.close();
+                    }
+                }
+                catch (Exception ex) {
+                    Log.d("rearrangeBoards",ex.getMessage());
                 }
             }
         }
     }
     private void saveBitmap(String name,Bitmap b, Context context) {
-        File mypath=new File(context.getFilesDir(), name);
+        File mypath = new File(context.getDir("boards", Context.MODE_PRIVATE),name);
 
         FileOutputStream fos = null;
         try {
             fos = new FileOutputStream(mypath);
-            b.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            b.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-                fos.close();
+                if(fos != null)
+                    fos.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        File directory = context.getDir("boards", Context.MODE_PRIVATE);
+        File[] files = directory.listFiles();
+        directory = context.getFilesDir();
+        if(files!=null)
+            Log.d("Save Bitmap",files.length+"");
     }
 }
