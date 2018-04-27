c7b6e1c68eac5675eff34fca96a05574fe2ebef5
Boscatov <ayumamontov@gmail.com>
2018-04-27 10:26:39 +0300

    Add achivements

--- Newgame/app/src/main/java/sudoku/newgame/datahelpers/UserTime.java
+++ Newgame/app/src/main/java/sudoku/newgame/datahelpers/UserTime.java
@@ -14,11 +14,13 @@ public class UserTime {
         this.username = username;
     }
     public void addToDataBase(String dim, String dif) {
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
         DatabaseReference leader = databaseReference.child("leaderboard").child(dim).child(dif);
         leader.push().setValue(this);
     }
     public static void addCreationTime(String dim, String dif, long time) {
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
         DatabaseReference creation = databaseReference.child("generation_time").child(dim).child(dif);
         creation.push().setValue(time);
